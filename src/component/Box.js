74940de7255080d442b7a83f61bb33d2bbff5b90
JiHunHeo <99526220+JiHunHeo@users.noreply.github.com>
2022-08-19 21:38:47 +0900

    img 태그 alt 추가

--- src/component/Box.js
+++ src/component/Box.js
@@ -10,7 +10,7 @@ const Box = (props) => {
     return (
         <div className={`box ${result}`}>
             <h1>{props.title}</h1>
-            <img className="item-img" src={props.item && props.item.img}></img>
+            <img className="item-img" src={props.item && props.item.img} alt="가위바위보관련이미지"></img>
             <h2>{result}</h2>
         </div>
     );
