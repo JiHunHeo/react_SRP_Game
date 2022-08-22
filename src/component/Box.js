ce8c03cb0da0019616656f7e3d79f81fa4108bec
JiHunHeo <99526220+JiHunHeo@users.noreply.github.com>
2022-08-22 21:06:55 +0900

    기본 이미지 추가

--- src/component/Box.js
+++ src/component/Box.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 const Box = (props) => {
     let result;
+    let def;
     if (props.title === 'Computer' && props.result !== 'Tie' && props.result !== '') {
         result = props.result === 'Win' ? 'Lose' : 'Win';
     } else {
         result = props.result;
     }
+    if (props.title === 'Computer' || props.title === 'You'){
+        def = 'https://www.hooni.net/xe/files/attach/images/203/452/088/fd317da58bf81b38eb29c03842ddcf1b.jpg';
+    }
     return (
         <div className={`box ${result}`}>
             <h1>{props.title}</h1>
-            <img className="item-img" src={props.item && props.item.img} alt="가위바위보"></img>
+            <img className="item-img" src={(props.item && props.item.img) || def} alt="가위바위보"></img>
             <h2>{result}</h2>
         </div>
     );
